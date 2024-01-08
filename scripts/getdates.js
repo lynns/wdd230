c14bad53a7d3e19ece5f3c3ecaf51b584ccff85e
Stephen Lynn <stephen.g.lynn@gmail.com>
2024-01-08 12:39:34 -0700

    Update layout to match rubric

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,12 +2,5 @@ const date = new Date();
 const yearString = date.toLocaleDateString('en-US', {
     year: 'numeric'
 });
-const dateString = date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: '2-digit'
-});
 document.querySelector("#copyrightYear").innerHTML = yearString;
-document.querySelector("#lastModified").innerHTML = `Last Modified: ${dateString}`;
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
