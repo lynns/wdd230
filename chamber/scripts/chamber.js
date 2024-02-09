dc4ca959f0ec42f45870e39f22b28eafea570baa
Stephen Lynn <stephen.g.lynn@gmail.com>
2024-02-09 13:50:12 -0700

    Add dark mode

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,4 @@
+/// Footer updates
 const date = new Date();
 const yearString = date.toLocaleDateString('en-US', {
     year: 'numeric'
@@ -5,10 +6,19 @@ const yearString = date.toLocaleDateString('en-US', {
 document.querySelector("#CopyrightYear").innerHTML = yearString;
 document.querySelector("#LastUpdated").innerHTML = document.lastModified;
 
-// Responsive Menu
+/// Responsive Menu
 const nav = document.querySelector("nav");
 const menuButton = document.querySelector("#MenuButton");
 
 menuButton.addEventListener("click", () => {
     nav.classList.toggle("menuOpen");
+});
+
+
+/// Dark Mode Support
+const darkModeButton = document.querySelector("#DarkModeButton");
+const body = document.querySelector("body");
+
+darkModeButton.addEventListener('click', () => {
+    body.classList.toggle("darkMode");
 });
